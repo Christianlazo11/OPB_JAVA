e98e31bebc77f4c4a3883605f7fa4574f5e25277
Christian Lazo <christian-lazo-u@hotmail.com>
2023-06-16 17:13:53 -0500

    Add item01

---
+++ src/com/item01/item01.java
@@ -0,0 +1,30 @@
+package com.item01;
+
+public class item01 {
+    public static void main(String[] args) {
+
+        byte var01 = 5;
+        short var02 = 55;
+        int var03 = 555;
+        long var04 = 5555;
+
+        float var05 = 5.5f;
+        double var06 = 55.55d;
+
+        char var07 = 'X';
+        String var08 = "OpenBootcamp";
+
+        boolean isItem = false;
+        boolean isCorrect = true;
+
+        System.out.println("Byte => " + var01);
+        System.out.println("Short => " + var02);
+        System.out.println("Int => " + var03);
+        System.out.println("Long => " + var04);
+        System.out.println("Float => " + var05);
+        System.out.println("Double => " + var06);
+        System.out.println("Char => " + var07);
+        System.out.println("String => " + var08);
+        System.out.println("Booleans => "  + isItem + " | "+isCorrect);
+    }
+}
