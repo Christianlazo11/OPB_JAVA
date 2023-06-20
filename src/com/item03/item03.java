46a1ae0bd84e61958a3c42b06586362ecb276336
Christian Lazo <christian-lazo-u@hotmail.com>
2023-06-20 11:55:59 -0500

    Add item03

---
+++ src/com/item03/item03.java
@@ -0,0 +1,23 @@
+package com.item03;
+
+public class item03 {
+
+    public static void main(String[] args) {
+
+        String[] text = {"hi", "how", "are", "you", "today"};
+
+        String result = concatText(text);
+
+        System.out.println("El resultado es: " + result);
+    }
+
+    static String concatText(String[] data) {
+        String resultData = "";
+
+        for(String n : data) {
+            resultData += n + " ";
+        }
+
+        return resultData;
+    }
+}
