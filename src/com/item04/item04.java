db387603fe6fcb21f26655ba9f2737972a76648f
Christian Lazo <christian-lazo-u@hotmail.com>
2023-06-23 22:06:57 -0500

    Add item04 finish

--- src/com/item04/item04.java
+++ src/com/item04/item04.java
@@ -8,8 +8,45 @@ public class item04 {
         SmartDevice device02 = new SmartPhone("MotoG60", "Motorola", 128, 6, 6000, 350, "Snapdragon 732G", "Android12", 108);
         SmartDevice device03 = new SmartWatch("Xiaomi redmi watch 2 lite", "Xiaomi", 1, 1, 256, 60, "IP40", "IP56", true);
 
+        SmartDevice device04 = new SmartDevice();
+        SmartPhone device05 = new SmartPhone();
+        SmartWatch device06 = new SmartWatch();
+
+        device04.setName("IPAD Pro");
+        device04.setBrand("Apple");
+        device04.setStorage(128);
+        device04.setRam(8);
+        device04.setBattery(8000);
+        device04.setPrice(1000);
+
+        device05.setName("Redmi note 15");
+        device05.setBrand("Xiaomi");
+        device05.setStorage(128);
+        device05.setRam(6);
+        device05.setBattery(5000);
+        device05.setPrice(400);
+        device05.setSo("Android 12");
+        device05.setCpu("Snapdragon 820");
+        device05.setCam(80);
+
+        device06.setName("Galaxy Watch 4");
+        device06.setBrand("Samsung");
+        device06.setStorage(16);
+        device06.setRam(2);
+        device06.setBattery(247);
+        device06.setPrice(150);
+        device06.setDustResistent("IP68");
+        device06.setHealthMonitoring(true);
+        device06.setWaterResistance("IP68");
+
+
+
+
         System.out.println(device01);
         System.out.println(device02);
         System.out.println(device03);
+        System.out.println(device04);
+        System.out.println(device05);
+        System.out.println(device06);
     }
 }
